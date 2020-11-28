83d1361e66efad1b7f170ebe97fe928e2198e095
jashanmeet <74665255+jashanmeet@users.noreply.github.com>
2020-11-28 19:45:17 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -47,10 +47,10 @@ function setup() {
   ground.x = ground.width /2;
   ground.velocityX = -(6 + 3*score/100);
   
-  gameOver = createSprite(Width/2,Height/2);
+  gameOver = createSprite(width/2,height/2);
   gameOver.addImage(gameOverImg);
   
-  restart = createSprite(Width/2,Height/2+50);
+  restart = createSprite(width/2,height/2+50);
   restart.addImage(restartImg);
   
   gameOver.scale = 0.5;
@@ -126,7 +126,7 @@ function draw() {
 function spawnClouds() {
   //write code here to spawn the clouds
   if (frameCount % 60 === 0) {
-    var cloud = createSprite(600,Height-200,40,10);
+    var cloud = createSprite(600,height-200,40,10);
     //cloud.y = Math.round(random(80,120));
     cloud.addImage(cloudImage);
     cloud.scale = 0.5;
